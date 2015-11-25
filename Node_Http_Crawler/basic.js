fd7dba2fd4879b2568c0efc0736d83c2b39835d0
Eason_Xu <eason820720@163.com>
2015-11-25 10:37:46 +0800

    11-25

---
+++ Node_Http_Crawler/basic.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+
+if (process.argv.length <= 2) {
+    console.log(process.argv);
+    process.exit(-1);
+}
+
+var url = process.argv[2];
+
+http.get(url, function(res) {
+    console.log("Got response: " + res.statusCode);
+    var content = '';
+    res.on('data', function(chunk) {
+        console.log('chunk ' + chunk.length);
+        content += chunk;
+    });
+    res.on('end', function() {
+        console.log('end');
+        console.log(content.length);
+        console.log(content);
+    });
+}).on('error', function(e) {
+    console.log("Got error: " + e.message);
+});
+
