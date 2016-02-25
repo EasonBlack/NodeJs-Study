7721d95bd987186b4906924c149bedcbdde80d69
Eason_Xu <eason820720@163.com>
2016-02-25 14:12:04 +0800

    get chinese from files

--- Node_File/get_chinese_in_files/app_tools.js
+++ Node_File/get_chinese_in_files/app_tools.js
@@ -1,7 +1,5 @@
 var fs = require('fs');
 
-
-
 var _file = 'F:\\Temp2';
 //var _file = '';
 var _allwords = [];
@@ -23,7 +21,9 @@ function intoFolders(path) {
             if(stats.isDirectory()) {
                 intoFolders(path + '\\' + f);
             } else {
-                readFile(path + '\\' +f );
+                if(f.indexOf('json')== -1 && f.indexOf('less')== -1 ) {
+                    readFile(path + '\\' +f );
+                }
             }
 
         });
@@ -39,14 +39,23 @@ function readFile(path) {
     //\u3001 ¡¢
     var _regex = />([\u4E00-\u9FFF\u3400-\u4DFF\uFF0C\u3002\u3001]+)</g;
     var match;
-    var _count = 0;
     while ( (match = _regex.exec(source)) !== null) {
         if(_allwords.indexOf(match[1])==-1) {
             _allwords.push(match[1])
             _words.push(match[1]);
         }
-        _count++;
     }
+
+    var _regex_2 = /["']([\u4E00-\u9FFF\u3400-\u4DFF\uFF0C\u3002\u3001]+)["']/g;
+    var match_2;
+    while ( (match_2 = _regex_2.exec(source)) !== null) {
+        if(_allwords.indexOf(match_2[1])==-1) {
+            _allwords.push(match_2[1])
+            _words.push(match_2[1]);
+        }
+    }
+
+
     if(_words.length) {
         _words.unshift('//start: '+ path )
         _words.push('//end: '+ path );
@@ -55,7 +64,12 @@ function readFile(path) {
 
     //fs.writeFile('array.txt',_words, 'utf-8');
     _words.forEach(function(v) {
-        output.write(v + '\n', 'utf-8');
+        if(v.indexOf('start')==-1 && v.indexOf('end')==-1 && v !='\n' ) {
+            output.write('\"'+ '' +'\":'+'\"' + v  + '\",'+ '\n', 'utf-8');
+        } else {
+            output.write( v + '\n', 'utf-8');
+        }
+
     });
 }
 
