deb8e7f7edb8374ea7f46dcc4333ba7cf6782791
Eason_Xu <eason820720@163.com>
2016-03-03 15:53:07 +0800

    fs

---
+++ Node_CMS/node_express_angular/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var uuid = require('node-uuid');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.listen(3000, function () {
+    console.log('connect success!');
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/vendor', express.static(path.join(__dirname, '../../vendor')));
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+app.post('/update', function (req, res) {
+    //console.log(req.body);
+    var name = req.body.name;
+    var img = req.body.img;
+    var dataBuffer = new Buffer(img, 'base64');
+    fs.writeFile(uuid.v1() + ".png", dataBuffer, function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("保存成功！");
+        }
+    });
+});
