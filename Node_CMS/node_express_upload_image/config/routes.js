adff088a29c44a816fc530c29018fa4674809732
Eason_Xu <eason820720@163.com>
2016-03-11 10:46:05 +0800

    upload file

--- Node_CMS/node_express_upload_image/config/routes.js
+++ Node_CMS/node_express_upload_image/config/routes.js
@@ -1,6 +1,7 @@
 
 var appCtrl = require('../controller/app.ctrl');
 var multer  = require('multer');
+
 //var upload = multer({ dest: 'uploads/' })
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,5 +17,8 @@ var upload = multer({ storage: storage });
 module.exports = function(app){
     app.get('/', appCtrl.getHtml );
     app.post('/upload', upload.single('file_aa'), appCtrl.setPic);
+
+    app.get('/2', appCtrl.getHtml2 );
+    app.post('/upload2', appCtrl.setPic2);
 }
 
--- Node_CMS/node_express_upload_image/controller/app.ctrl.js
+++ Node_CMS/node_express_upload_image/controller/app.ctrl.js
@@ -7,7 +7,24 @@ exports.getHtml = function(req, res) {
     res.sendfile(path.join(__dirname , '../public/index.html'));
 }
 
+exports.getHtml2 = function(req, res) {
+    res.sendfile(path.join(__dirname , '../public/index_better.html'));
+}
+
 exports.setPic = function(req,res) {
-    console.log(req.body) // form fields
-    console.log(req.file) // form files
+    console.log(req.body)
+    console.log(req.file)
+}
+
+exports.setPic2 = function(req,res) {
+    console.log(req.body);
+    var dataBuffer = new Buffer(req.body.img, 'base64');
+    var _guidName = uuid.v1();
+    fs.writeFile('uploads/' + _guidName + ".png", dataBuffer, function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("保存成功！");
+        }
+    });
 }
