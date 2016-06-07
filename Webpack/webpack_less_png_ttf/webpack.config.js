d73a3315df293560fb3eaff5408de180209ba766
easn xu <eason820720@gmail.com>
2016-06-07 17:11:48 +0800

    webpack

---
+++ Webpack/webpack_less_png_ttf/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: [
+        "./client.js",
+        "./style/main.less"
+    ],
+    output: {
+        path: "./build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("../style/[name].css")
+    ]
+}
