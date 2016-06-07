c862895fdc90d7cc0bf137dfc3367dfc8951edfa
easn xu <eason820720@gmail.com>
2016-06-07 14:40:03 +0800

    less mini

---
+++ Webpack/webpack_less_minify/app.ctrl.js
@@ -0,0 +1,7 @@
+var angular = require("angular");
+
+angular.module("app")
+.controller('appCtrl', ['$scope',function ($scope) {
+    $scope.studentname = 'xxxx';
+    $scope.studentage = '111';
+}]);
